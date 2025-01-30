f93dbc698e722a4acf7ef1fe0753f11c493f227c
Juan Segundo <juansepincha@gmail.com>
2025-01-29 23:30:45 -0300

    light mode

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         <SessionClient>
           <SidebarProvider>
             <UiSidebarProvider>
-              <ThemeProvider attribute="class" defaultTheme="system">
+              <ThemeProvider attribute="class" defaultTheme="light">
                 <AppSidebar />
                 <SidebarInset>
                   <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
